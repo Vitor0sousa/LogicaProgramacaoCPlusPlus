a6327a7823eed3704427fbe01f4abac66837f4fb
vitor0sousa <157727867+Vitor0sousa@users.noreply.github.com>
2024-08-29 14:29:18 -0300

    Add files via upload
    html

---
+++ Dark_Light/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const switcher = document.querySelector('button');
+
+switcher.addEventListener('click', function(){
+document.body.classList.toggle('dark-theme')
+
+
+var className = document.body.className;
+if(className == 'light-theme'){
+    this.textContent = 'dark';
+}
+else{
+    this.textContent='light';
+}
+console.log('current class name:'+ className);
+
+})
